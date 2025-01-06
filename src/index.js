1a1dfd24c25f3b5fdd078d7aa86568cd4f4b2c96
ddonkov <ddonkov@asteasolutions.com>
2025-01-06 15:19:05 +0200

    Add simple server with endpoint

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const PORT = 3000;;
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.send('Hello, Devops Project!\n');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
