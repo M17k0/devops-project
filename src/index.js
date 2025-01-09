9220c7d9c5cd5a91fa4f6c830caa35bca89ae6d0
m17k0 <m17k018@gmail.com>
2025-01-09 22:07:43 +0200

    Remove vercel

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 
+const PORT = 3000;
+
 const app = express();
 
 app.get('/', (_req, res) => {
   res.send('Hello, Devops Project!\n');
 });
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
